dbb2024df6452189b9b85f5207bd5899a6c0ce99
ignasa007 <jasraj001@ntu.edu.sg>
2025-07-10 11:55:55 +0100

    last layer identity activation

--- models/vision/simple_cnn.py
+++ models/vision/simple_cnn.py
@@ -26,10 +26,11 @@ class SimpleCNN(nn.Module):
         
         # CONV LAYERS
         self.conv_layers = nn.ModuleList()
-        # Input layer; infer input size upon first forward pass
-        kwargs = make_kwargs(kernel_size=kernel_sizes.pop(0), stride=strides.pop(0), padding=paddings.pop(0), bias=biases.pop(0))
-        self.conv_layers.append(nn.LazyConv2d(num_channels[0], **kwargs))
-        # Hidden layers
+        if num_conv_layers > 0:
+            # First layer; infer input size upon first forward pass
+            kwargs = make_kwargs(kernel_size=kernel_sizes.pop(0), stride=strides.pop(0), padding=paddings.pop(0), bias=biases.pop(0))
+            self.conv_layers.append(nn.LazyConv2d(num_channels[0], **kwargs))
+        # Following layers
         for args in zip(num_channels[:-1], num_channels[1:], kernel_sizes, strides, paddings, biases):
             in_channels, out_channels, kernel_size, stride, padding, bias = args
             kwargs = make_kwargs(kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
@@ -37,7 +38,7 @@ class SimpleCNN(nn.Module):
 
         # POOLING LAYERS
         pooler_names = interpret_args(cfg.architecture.conv.poolers.names, to=str, num_layers=num_conv_layers)
-        # Assuming square pooling kernels
+        # Assuming square kernels
         kernel_sizes = interpret_args(cfg.architecture.conv.poolers.sizes, to=int, num_layers=num_conv_layers)
         strides = interpret_args(cfg.architecture.conv.poolers.strides, to=int, num_layers=num_conv_layers)
         paddings = interpret_args(cfg.architecture.conv.poolers.paddings, to=int, num_layers=num_conv_layers)
@@ -64,51 +65,52 @@ class SimpleCNN(nn.Module):
         self.flatten = nn.Flatten()
         num_features = interpret_args(cfg.architecture.fcn.hidden_dims, to=int, num_layers=None)
         num_fcn_layers = len(num_features)
-        biases = interpret_args(cfg.architecture.fcn.biases, to=bool, num_layers=num_fcn_layers+1)
+        # activations will be the same length as the number of hidden layers
+        activations = interpret_args(cfg.architecture.fcn.activations, to=str, num_layers=num_fcn_layers)
+        if hasattr(cfg.dataset, "output_dim"):
+            num_features += [cfg.dataset.output_dim]
+            num_fcn_layers += 1
+            activations.append("Identity")
+        # biases will be the same length as the total number of layers (including output)
+        # NOTE: bias in last layer unnecessary for CE loss, but can be relevant when using MSE
+        biases = interpret_args(cfg.architecture.fcn.biases, to=bool, num_layers=num_fcn_layers)
 
         # FCN LAYERS
         self.fcn_layers = nn.ModuleList()
-        # Input layer; infer input size upon first forward pass
-        kwargs = make_kwargs(bias=biases.pop(0))
-        self.fcn_layers.append(nn.LazyLinear(out_features=num_features[0], **kwargs))
-        # Hidden layers
+        if num_fcn_layers > 0:
+            # First layer; infer input size upon first forward pass
+            kwargs = make_kwargs(bias=biases.pop(0))
+            self.fcn_layers.append(nn.LazyLinear(out_features=num_features[0], **kwargs))
+        # Following layers
         for args in zip(num_features[:-1], num_features[1:], biases):
             in_features, out_features, bias = args
             kwargs = make_kwargs(bias=bias)
             self.fcn_layers.append(nn.Linear(in_features, out_features, **kwargs))
-        # Output layer
-        kwargs = make_kwargs(bias=biases[-1])
-        self.fcn_layers.append(nn.Linear(num_features[-1], cfg.dataset.output_dim, **kwargs))
 
         # ACTIVATION FUNCTIONS
-        activations = interpret_args(cfg.architecture.fcn.activations, to=str, num_layers=num_fcn_layers)
         self.fcn_activations = nn.ModuleList()
         for name in activations:
             self.fcn_activations.append(get_activation(name))
 
     def reset_parameters(self):
 
-        for layer in sum((
+        for module in sum((
             self.conv_layers,
             self.conv_normalization_layers,
             self.fcn_layers
         )):
-            layer.reset_parameters()
+            module.reset_parameters()
 
     def forward(self, x):
 
         for convolution, pooling, normalization, activation in zip(
             self.conv_layers, self.conv_pooling_layers, self.conv_normalization_layers, self.conv_activations
         ):
-            # Order of ops is up to debate. My reasoning:
-            #   1. Pooling commutes with monotonic activations => put pooling first to save compute.
-            #   2. Normalization is put in diff places by diff authors.
-            #       - Following the original paper, we perform norm before activation.
-            #       - But after pooling to save compute, and retain its effect in the output.
+            # Order of ops is up to debate.
             x = convolution(x)
-            x = pooling(x)
             x = normalization(x)
             x = activation(x)
+            x = pooling(x)
 
         x = self.flatten(x)
         for linear, activation in zip(self.fcn_layers, self.fcn_activations):
